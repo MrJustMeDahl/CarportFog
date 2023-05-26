401d21ee8a04798ac43ae6de218300e3a438f32f
MrJustMeDahl <nico.dahl93@gmail.com>
2023-05-26 17:25:44 +0200

    Fixed bug that orderstatus for paid orders were not updated in java

--- src/main/java/dat/backend/control/OrderAndPayment.java
+++ src/main/java/dat/backend/control/OrderAndPayment.java
@@ -57,6 +57,7 @@ public class OrderAndPayment extends HttpServlet
             for (int i = 0; i < list.size(); i++) {
                 if (list.get(i).getOrderID()==idCheck){
                     order = list.get(i);
+                    order.setOrderStatus("payed");
                     request.setAttribute("order",order);
                 }
             }
